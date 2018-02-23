eec1dde21f623c619550122eb9ddf63f4b164b02
Emre <emrebym@gmail.com>
2018-02-23 14:44:03 +0100

    Added tests
    Added more test methods to CreateGameTest

--- app/src/test/java/de/emrebayram/skatbrderbren/CreateGameTest.java
+++ app/src/test/java/de/emrebayram/skatbrderbren/CreateGameTest.java
@@ -23,4 +23,22 @@ public class CreateGameTest {
         game.addPlayer(new Player("test"));
         assertThat(initalCount < game.getPlayerCount(), is(true));
     }
+
+    @Test
+    public void createGame_RemovePlayer_DecreasesCountOfPlayers() {
+        Game game = new Game(301);
+        Player testPlayer = new Player("test");
+        game.addPlayer(testPlayer);
+        int initalCount = game.getPlayerCount();
+        game.removePlayer(testPlayer);
+        assertThat(initalCount > game.getPlayerCount(), is(true));
+    }
+
+    @Test
+    public void createGame_ChangeScoreToWin_ChangesScoreToWinOfGame() {
+        Game game = new Game(301);
+        int newScoreToWin = 501;
+        game.setScoreToWin(newScoreToWin);
+        assertThat(newScoreToWin == game.getScoreToWin(), is(true));
+    }
 }
